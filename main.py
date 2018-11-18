bbc692e3bbc45c3d442da185094d37dfd93b7b72
bcbabrich <39421140+bcbabrich@users.noreply.github.com>
2018-11-18 18:08:20 -0500

    added train/val splits, all experiment functionality in nn.py now.

--- main.py
+++ main.py
@@ -2,55 +2,6 @@ from nn import runExperiment
 from poisson_sampling import getPoints
 import random
 
-# RUN NETWORK EXPERIMENT
-# this function will run the network in nn.py with the given set of hyperparameters
-# and return the highest accuracy found along with the corresponding hyperparameters
-# IN: a list of tuples containing hyperparameters, 
-#     an int of desired number of points to be run
-# OUT: a tuple containing highest_accuracy, best_learning_rate, and best_hidden_size
-# NOTE: we use the names "points" and "hyperparamaters" interchangeably
-def run_network_experiment(desired_num_points, points) :
-    highest_accuracy = 0
-    best_learning_rate = 0
-    best_hidden_size =  0
-    
-    p = 0 # count number of points being used (for printing purposes)
-
-    # shuffle points
-    random.shuffle(points)
-
-    # EXPERIMENT RUN
-    # only run desired_num_points of
-    # this takes care of the case where generate_poisson_points
-    # returned a list of slightly too many points (< 5)
-    for i in range(desired_num_points) :
-        # draw a random point
-        point = points[i]
-
-        learning_rate = point[0]
-        hidden_size = int(round(point[1]))
-
-        #run nn with generated hyperparameters
-        accuracy = runExperiment(learning_rate, hidden_size)
-
-        # print every tenth result
-        '''
-        if p % 5 == 0 :
-            print('point number',p)
-            print('accuracy',accuracy)
-            print('learning_rate',learning_rate)
-            print('hidden_size',hidden_size)
-        '''
-
-        if accuracy > highest_accuracy :
-            highest_accuracy = accuracy
-            best_learning_rate = learning_rate
-            best_hidden_size = hidden_size
-
-        p += 1
-
-    return (highest_accuracy, best_learning_rate, best_hidden_size)
-
 # GENERATE RANDOM BATCH
 # function generates a list of tuples of size desired_num_points, each
 # of which are randomly generated values in the ranges learning_rate_range
@@ -104,23 +55,21 @@ def generate_Poisson_batch(desired_num_points, learning_rate_range, hidden_size_
 ##############################################################
 ################# MAIN METHOD ################################
 ##############################################################
-# how many points we can "afford" to test for
-desired_num_points = 25
-
-# generate hyperparameters ... using hyper-hyperparameters
+# define hyperparameters ... using "hyper-hyperparameters"
 learning_rate_range = 1
 hidden_size_range = 1000
+desired_num_points = 25 # how many points we can "afford" to test for
 
 # generate completely random points
 random_points = generate_random_batch(desired_num_points, learning_rate_range, hidden_size_range)
 
 # run experiment with randomly generated points
 print('STARTING RANDOM EXPERIMENT')
-highest_accuracy, best_learning_rate, best_hidden_size = run_network_experiment(desired_num_points, random_points)
+test_accuracy, best_learning_rate, best_hidden_size = runExperiment(random_points, desired_num_points)
 
 # print results 
 print('RANDOM EXPERIMENT DONE.')
-print('highest_accuracy: ',highest_accuracy,'%')
+print('test_accuracy: ',test_accuracy,'%')
 print('best_learning_rate',best_learning_rate)
 print('best_hidden_size',best_hidden_size)
 
@@ -129,10 +78,10 @@ print('STARTING POISSON EXPERIMENT')
 poisson_points = generate_Poisson_batch(desired_num_points, learning_rate_range, hidden_size_range)
 
 # run experiment with randomly generated points
-highest_accuracy, best_learning_rate, best_hidden_size = run_network_experiment(desired_num_points, random_points)
+test_accuracy, best_learning_rate, best_hidden_size = runExperiment(poisson_points, desired_num_points)
 
 # print results 
 print('POISSON EXPERIMENT DONE.')
-print('highest_accuracy: ',highest_accuracy,'%')
+print('test_accuracy: ',test_accuracy,'%')
 print('best_learning_rate',best_learning_rate)
 print('best_hidden_size',best_hidden_size)
